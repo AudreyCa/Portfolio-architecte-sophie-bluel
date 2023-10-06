60389e84c6fb6f3aed2aed1391d728525e33b169
Aud Can <a.cannesson5@gmail.com>
2023-10-06 18:19:41 +0200

    ajout page edition HTML/CSS sans modale

---
+++ FrontEnd/edit.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tous = document.getElementById("tous");
+    // const objects = document.getElementById("objects");
+    // const appartements = document.getElementById("appartements");
+    // const hotelsRestos = document.getElementById("hotels_restaurants");
+    const galerie = document.querySelector(".gallery");
+
+    // Au chargement de la page, on affiche tous les projets :
+    fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            // console.log(element);
+            let projet = document.createElement('figure');
+            projet.innerHTML =`<img src=${element.imageUrl} alt=${element.title}>
+            <figcaption>${element.title}</figcaption>`;
+            galerie.appendChild(projet);
+        });
+    });
+
+});
